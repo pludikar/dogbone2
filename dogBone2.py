9962a62d25418887f355a407b8b0d22b8d1c13e4
peter ludikar <peter@ludikar.com>
2018-01-12 19:55:45 -0500

    Initial commit

---
+++ dogBone2.py
@@ -0,0 +1,334 @@
+#Author-Peter Ludikar
+#Description-An Add-In for making dog-bone fillets.
+
+# This version is a proof of concept 
+
+# I've completely revamped the dogbone add-in by Casey Rogers and Patrick Rainsberry and David Liu
+# some of the original utilities have remained, but mostly everything else has changed.
+
+# The original add-in was based on creating points and extruding - I found using sketches and extrusion to be very heavy 
+# on processing resources, so this version has been designed to create dogbones directly by using a hole tool. So far the
+# the performance of this approach is day and night compared to the original version. 
+
+# Select the face you want the dogbones to drop from. Specify a tool diameter and a radial offset.
+# The add-in will then create a dogbone with diamater equal to the tool diameter plus
+# twice the offset (as the offset is applied to the radius) at each selected edge.
+
+# to do:
+# 1. Selection of multiple faces and selecting/deselecting target edges (intention is to use attributes to relate
+#    edges to faces, probably collected during onFaceSelect events - that way the prepopulated entities don't have to be 
+#    recalculated on every mouse move
+# 2. ... who knows
+ 
+from collections import defaultdict
+
+import adsk.core, adsk.fusion
+import math
+import traceback
+import uuid
+
+import time
+from . import utils as dbutils
+
+
+class DogboneCommand(object):
+    COMMAND_ID = "dogboneBtn"
+
+    def __init__(self):
+        self.app = adsk.core.Application.get()
+        self.ui = self.app.userInterface
+
+        self.offStr = "0"
+        self.offVal = None
+        self.circStr = "0.25 in"
+        self.circVal = None
+        self.edges = []
+        self.benchmark = False
+
+        self.handlers = dbutils.HandlerHelper()
+
+    def addButton(self):
+        # clean up any crashed instances of the button if existing
+        try:
+            self.removeButton()
+        except:
+            pass
+
+        # add add-in to UI
+        buttonDogbone = self.ui.commandDefinitions.addButtonDefinition(
+            self.COMMAND_ID, 'Dogbone', 'Creates dogbones at all inside corners of a face', 'Resources')
+
+        buttonDogbone.commandCreated.add(self.handlers.make_handler(adsk.core.CommandCreatedEventHandler,
+                                                                    self.onCreate))
+
+        createPanel = self.ui.allToolbarPanels.itemById('SolidCreatePanel')
+        buttonControl = createPanel.controls.addCommand(buttonDogbone, 'dogboneBtn')
+
+        # Make the button available in the panel.
+        buttonControl.isPromotedByDefault = True
+        buttonControl.isPromoted = True
+
+    def removeButton(self):
+        cmdDef = self.ui.commandDefinitions.itemById(self.COMMAND_ID)
+        if cmdDef:
+            cmdDef.deleteMe()
+        createPanel = self.ui.allToolbarPanels.itemById('SolidCreatePanel')
+        cntrl = createPanel.controls.itemById(self.COMMAND_ID)
+        if cntrl:
+            cntrl.deleteMe()
+
+    def onCreate(self, args:adsk.core.CommandCreatedEventArgs):
+        inputs = adsk.core.CommandCreatedEventArgs.cast(args)
+        argsCmd = adsk.core.Command.cast(args)
+
+        inputs = inputs.command.commandInputs
+
+        selInput0 = inputs.addSelectionInput(
+            'select', 'Face',
+            'Select a face to apply dogbones to all internal corner edges')
+#        selInput0.addSelectionFilter('LinearEdges')
+        selInput0.addSelectionFilter('Faces')
+        selInput0.setSelectionLimits(1,0)
+
+        inp = inputs.addValueInput(
+            'circDiameter', 'Tool Diameter', self.design.unitsManager.defaultLengthUnits,
+            adsk.core.ValueInput.createByString(self.circStr))
+        inp.tooltip = "Size of the tool with which you'll cut the dogbone."
+
+        inp = inputs.addValueInput(
+            'offset', 'Additional Offset', self.design.unitsManager.defaultLengthUnits,
+            adsk.core.ValueInput.createByString(self.offStr))
+        inp.tooltip = "Additional increase to the radius of the dogbone."
+
+        inputs.addBoolValueInput("benchmark", "Benchmark running time", True, "", self.benchmark)
+
+        # Add handlers to this command.
+        args.command.execute.add(self.handlers.make_handler(adsk.core.CommandEventHandler, self.onExecute))
+        args.command.selectionEvent.add(self.handlers.make_handler(adsk.core.SelectionEventHandler, self.onFaceSelect))
+        args.command.validateInputs.add(
+            self.handlers.make_handler(adsk.core.ValidateInputsEventHandler, self.onValidate))
+#==============================================================================
+#         args.command.inputChanged.add(
+#             self.handlers.make_handler(adsk.core.InputChangedEventHandler, self.onChangeInput))
+#             
+#             
+#     def onChangeInput(self, args:adsk.core.InputChangedEventArgs):
+#         inputs = {inp.id: inp for inp in args.inputs}
+#         activeInput = args.input
+#         if activeInput.id != 'select':
+#             return
+#         
+#         edges = []
+# #        if not self.faces:
+# #            self.faces = {}
+#         for i in range(inputs['select'].selectionCount):
+#             entity = inputs['select'].selection(i).entity
+#             if entity.objectType == adsk.fusion.BRepEdge.classType():
+#                 edges.append(entity)
+#             elif entity.objectType == adsk.fusion.BRepFace.classType():
+#                 face = entity
+#         try:
+#             self.faces.append([face, edges])
+#         except:
+#             self.faces = []
+#             self.faces.append([face, edges])
+#==============================================================================
+
+
+    def parseInputs(self, inputs):
+        inputs = {inp.id: inp for inp in inputs}
+
+        self.circStr = inputs['circDiameter'].expression
+        self.circVal = inputs['circDiameter'].value
+        self.offStr = inputs['offset'].expression
+        self.offVal = inputs['offset'].value
+        self.benchmark = inputs['benchmark'].value
+
+        self.edges = []
+        self.faces = []
+        
+        for i in range(inputs['select'].selectionCount):
+            entity = inputs['select'].selection(i).entity
+            if entity.objectType == adsk.fusion.BRepEdge.classType():
+                self.edges.append(entity)
+            elif entity.objectType == adsk.fusion.BRepFace.classType():
+                self.faces.append(entity)
+
+
+    def onExecute(self, args):
+        start = time.time()
+
+        self.parseInputs(args.firingEvent.sender.commandInputs)
+        self.createConsolidatedDogbones()
+
+        if self.benchmark:
+            dbutils.messageBox("Benchmark: {:.02f} sec processing {} edges".format(
+                time.time() - start, len(self.edges)))
+
+    def onValidate(self, args):
+        cmd = adsk.core.ValidateInputsEventArgs.cast(args)
+        cmd = args.firingEvent.sender
+
+        for input in cmd.commandInputs:
+            if input.id == 'select':
+                if input.selectionCount < 1:
+                    args.areInputsValid = False
+            elif input.id == 'circDiameter':
+                if input.value <= 0:
+                    args.areInputsValid = False
+                    
+    def onFaceSelect(self, args):
+        eventArgs = adsk.core.SelectionEventArgs.cast(args)
+        # Check which selection input the event is firing for.
+        activeIn = eventArgs.firingEvent.activeInput
+        if activeIn.id != 'select':
+            return
+        selected = eventArgs.selection
+        selectedEntity = selected.entity
+        faceEdges = adsk.core.ObjectCollection.create()
+
+        if selectedEntity.objectType != adsk.fusion.BRepFace.classType():
+            eventArgs.activeInput.clearSelectionFilter()
+            eventArgs.activeInput.addSelectionFilter('Faces')
+
+            return
+        
+        eventArgs.activeInput.addSelectionFilter('LinearEdges')
+
+        face = adsk.fusion.BRepFace.cast(selectedEntity)
+        faceNormal = dbutils.getFaceNormal(face)
+        
+        for edge in face.body.edges:
+            if edge.isDegenerate:
+                continue
+            try:
+                if edge.geometry.curveType != adsk.core.Curve3DTypes.Line3DCurveType:
+                    continue
+                vector = edge.startVertex.geometry.vectorTo(edge.endVertex.geometry)
+                if vector.isPerpendicularTo(faceNormal):
+                    continue
+                if edge.faces.item(0).geometry.objectType != adsk.core.Plane.classType():
+                    continue
+                if edge.faces.item(1).geometry.objectType != adsk.core.Plane.classType():
+                    continue              
+                if edge.startVertex not in face.vertices:
+                    if edge.endVertex not in face.vertices:
+                        continue
+                    else:
+                        vector = edge.endVertex.geometry.vectorTo(edge.startVertex.geometry)
+                if vector.dotProduct(faceNormal) >= 0:
+                    continue
+                if dbutils.getAngleBetweenFaces(edge) > math.pi:
+                    continue
+                faceEdges.add(edge)
+            except:
+                dbutils.messageBox('Failed at edge:\n{}'.format(traceback.format_exc()))
+        eventArgs.additionalEntities = faceEdges
+    
+
+    @property
+    def design(self):
+        return self.app.activeProduct
+
+    @property
+    def rootComp(self):
+        return self.design.rootComponent
+
+    @property
+    def originPlane(self):
+        return self.rootComp.xZConstructionPlane if self.yUp else self.rootComp.xYConstructionPlane
+
+    # The main algorithm
+    def createConsolidatedDogbones(self):
+        if not self.design:
+            raise RuntimeError('No active Fusion design')
+        holeInput = adsk.fusion.HoleFeatureInput.cast(None)
+
+        radius = self.circVal / 2 + self.offVal
+        offset = adsk.core.ValueInput.createByReal(radius/math.sqrt(2))
+        startTlMarker = self.design.timeline.markerPosition
+
+        for face in self.faces:
+#            face in an assembly context needs to be treated differently to a face that is not in a component
+            if face.assemblyContext:
+               comp = face.assemblyContext.sourceComponent
+               name = face.assemblyContext.name.split(':')[0]+':1'  #occurrence is supposed to take care of positioning
+               occ = self.rootComp.occurrences.itemByName(name)  # this is a work around - use 1st occurrence as proxy
+               face = face.nativeObject.createForAssemblyContext(occ)
+
+            else:
+               comp = self.rootComp
+               occ = None
+
+#            sketch = comp.sketches.add(comp.xYConstructionPlane, occ)  #used for fault finding
+            holes = adsk.fusion.HoleFeatures.cast(comp.features.holeFeatures)
+                
+            dbutils.clearFaceAttribs(self.design)
+            dbutils.setFaceAttrib(face)
+                
+            faceNormal = dbutils.getFaceNormal(face)
+            
+            for edge in self.edges:
+                #face becomes invalid after a hole is added - use attributes to find the right face
+                if not face.isValid:
+                    face = dbutils.refreshFace(self.design)
+                if edge.assemblyContext:
+                    #put face and edge into right context (1st component) - create proxies
+                    edge = edge.nativeObject.createForAssemblyContext(occ)
+                    face = face.nativeObject.createForAssemblyContext(occ)
+                    
+                startVertex = dbutils.getVertexAtFace(face, edge)
+                if not dbutils.isEdgeAssociatedWithFace(face, edge):
+                    continue
+                extentToEntity = dbutils.defineExtent(face, edge)
+                try:
+                    (edge1, edge2) = dbutils.getCornerEdgesAtFace(face, edge)
+                except: 
+                    dbutils.messageBox('Failed at findAdjecentFaceEdges:\n{}'.format(traceback.format_exc()))
+
+                initGuess = startVertex.geometry.copy()
+                #determine directions to translate the initGuess - needs to be inside the corner, not on the face
+                for edgeFace in edge.faces:
+                    dirVect = dbutils.getFaceNormal(edgeFace).copy()
+                    dirVect.normalize()
+                    dirVect.scaleBy(radius/math.sqrt(2))
+                    initGuess.translateBy(dirVect)
+#                sketch.sketchPoints.add(initGuess)        #for debugging 
+
+                #create hole attributes
+                holeInput = holes.createSimpleInput(adsk.core.ValueInput.createByReal(2*radius))
+                holeInput.tipAngle = adsk.core.ValueInput.createByString('180 deg')
+                holeInput.isDefaultDirection = True
+                holeInput.creationOccurrence = face.assemblyContext
+                holeInput.participantBodies = [face.body]
+                holeInput.setPositionByPlaneAndOffsets(face, initGuess, edge1, offset, edge2, offset)
+                holeInput.setOneSideToExtent(extentToEntity,False)
+                try: 
+                    hole = holes.add(holeInput)
+                    hole.name = 'dogbone'
+
+                except:
+                    dbutils.messageBox('Failed at create hole add:\n{}'.format(traceback.format_exc()))
+                    continue
+            endTlMarker = self.design.timeline.markerPosition-1
+            if endTlMarker - startTlMarker >0:
+                timelineGroup = self.design.timeline.timelineGroups.add(startTlMarker,endTlMarker)
+                timelineGroup.name = 'dogbone'
+                
+                
+
+dog = DogboneCommand()
+
+
+def run(context):
+    try:
+        dog.addButton()
+    except:
+        dbutils.messageBox(traceback.format_exc())
+
+
+def stop(context):
+    try:
+        dog.removeButton()
+    except:
+        dbutils.messageBox(traceback.format_exc())
---
+++ utils.py
@@ -0,0 +1,162 @@
+import math
+import traceback
+
+import adsk.core
+import adsk.fusion
+
+
+def getAngleBetweenFaces(edge):
+    # Verify that the two faces are planar.
+    face1 = edge.faces.item(0)
+    face2 = edge.faces.item(1)
+    if face1 and face2:
+        if face1.geometry.objectType != adsk.core.Plane.classType() or face2.geometry.objectType != adsk.core.Plane.classType():
+            return 0
+    else:
+        return 0
+
+    # Get the normal of each face.
+    ret = face1.evaluator.getNormalAtPoint(face1.pointOnFace)
+    normal1 = ret[1]
+    ret = face2.evaluator.getNormalAtPoint(face2.pointOnFace)
+    normal2 = ret[1]
+    # Get the angle between the normals.
+    normalAngle = normal1.angleTo(normal2)
+
+    # Get the co-edge of the selected edge for face1.
+    if edge.coEdges.item(0).loop.face == face1:
+        coEdge = edge.coEdges.item(0)
+    elif edge.coEdges.item(1).loop.face == face1:
+        coEdge = edge.coEdges.item(1)
+
+    # Create a vector that represents the direction of the co-edge.
+    if coEdge.isOpposedToEdge:
+        edgeDir = edge.startVertex.geometry.vectorTo(edge.endVertex.geometry)
+    else:
+        edgeDir = edge.endVertex.geometry.vectorTo(edge.startVertex.geometry)
+
+    # Get the cross product of the face normals.
+    cross = normal1.crossProduct(normal2)
+
+    # Check to see if the cross product is in the same or opposite direction
+    # of the co-edge direction.  If it's opposed then it's a convex angle.
+    if edgeDir.angleTo(cross) > math.pi/2:
+        angle = (math.pi * 2) - (math.pi - normalAngle)
+    else:
+        angle = math.pi - normalAngle
+
+    return angle
+
+def defineExtent(face, edge):
+    
+    faceNormal = adsk.core.Vector3D.cast(face.evaluator.getNormalAtPoint(face.pointOnFace)[1])
+    
+    if edge.startVertex in face.vertices:
+        endVertex = edge.endVertex
+    else:
+        endVertex = edge.startVertex
+    return endVertex
+
+    
+def correctedEdgeVector(edge, refVertex):
+    if edge.startVertex.geometry == refVertex.geometry:
+        return edge.startVertex.geometry.vectorTo(edge.endVertex.geometry)
+    else:
+        return edge.endVertex.geometry.vectorTo(edge.startVertex.geometry)
+    return False
+    
+
+def isEdgeAssociatedWithFace(face, edge):
+    
+    # have to check both ends - not sure which way around the start and end vertices are
+    if edge.startVertex in face.vertices:
+        return True
+    if edge.endVertex in face.vertices:
+        return True
+    return False
+    
+def getCornerEdgesAtFace(face, edge):
+    #not sure which end is which - so test edge ends for inclusion in face
+    if edge.startVertex in face.vertices:
+        startVertex = edge.startVertex
+    else:
+        startVertex = edge.endVertex 
+    #edge has 2 adjacent faces - therefore the face that isn't from the 3 faces of startVertex, has to be the top face edges
+#    returnVal = [edge1 for edge1 in edge.startVertex.edges if edge1 in face.edges]
+    returnVal = []
+    for edge1 in startVertex.edges:
+        if edge1 not in face.edges:
+            continue
+        returnVal.append(edge1)
+    if len(returnVal)!= 2:
+        raise NameError('returnVal len != 2')
+        
+    return (returnVal[0], returnVal[1])
+    
+def getVertexAtFace(face, edge):
+    if edge.startVertex in face.vertices:
+        return edge.startVertex
+    else:
+        return edge.endVertex
+    return False
+    
+def getFaceNormal(face):
+    return face.evaluator.getNormalAtPoint(face.pointOnFace)[1]
+    
+
+# Finds and returns two EDGES that form a corner adjacent to EDGE
+#def findCorner(edge):
+#    # XXX(dliu): Is there a way to get adjacent edges directly instead of going from edge => face => edges?
+#    faces = edge.faces
+#    edges0 = faces.item(0).edges
+#    edges1 = faces.item(1).edges
+#    for e0 in edges0:
+#        if e0 == edge:
+#            continue
+#        for e1 in edges1:
+#            if e1 == edge:
+#                continue
+#            a0, a1 = e0.startVertex, e0.endVertex
+#            b0, b1 = e1.startVertex, e1.endVertex
+#            if a0 == b0 or a0 == b1 or a1 == b0 or a1 == b1:
+#                return e0, e1
+#    raise RuntimeError("findCorner called on non-adjacent edges")
+    
+def messageBox(*args):
+    adsk.core.Application.get().userInterface.messageBox(*args)
+
+def clearFaceAttribs(design):
+    attribs = design.findAttributes("dogBone","dbFace")
+    if not attribs:
+        return
+    for attrib in attribs:
+        attrib.deleteMe()
+        
+def setFaceAttrib(face):
+    face.attributes.add("dogBone", "dbFace","1")
+    
+def refreshFace(design):
+    attribs = design.findAttributes("dogBone","dbFace")
+    if len(attribs) !=1:
+        return False
+    return attribs[0].parent
+    
+class HandlerHelper(object):
+    def __init__(self):
+        # Note: we need to maintain a reference to each handler, otherwise the handlers will be GC'd and SWIG will be
+        # unable to call our callbacks. Learned this the hard way!
+        self.handlers = []  # needed to prevent GC of SWIG objects
+
+    def make_handler(self, handler_cls, notify_method, catch_exceptions=True):
+        class _Handler(handler_cls):
+            def notify(self, args):
+                if catch_exceptions:
+                    try:
+                        notify_method(args)
+                    except:
+                        messageBox('Failed:\n{}'.format(traceback.format_exc()))
+                else:
+                    notify_method(args)
+        h = _Handler()
+        self.handlers.append(h)
+        return h
